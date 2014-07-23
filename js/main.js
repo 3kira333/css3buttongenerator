7e80a94cce2ae8cf25b24e5d83f01b0bfd8b1ef5
Andrey Korshunov <dron1702-1997@bk.ru>
2014-07-23 20:58:17 +0300

    Minor styles changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
+
+
+(function() {
+
+//variables
+var changeRadius = $('#slider1'),
+		changeBorder = $('#slider2'),
+		result = $('#result'),
+		maxRadius = 22.
+		minRadius = 0;
+
+//activate slider of border radius
 $('#slider1').slider({
-	range: 'min'
+	range: 'min',
+	min: 0,
+	max: 23,
+	value: 4,
+	slide: function(event, ui) {
+		var newRadius = changeRadius.slider('value');
+				// console.log(newRadius);
+		result.css({
+			'border-radius' : newRadius
+		})
+	}
 });
+
+
+
 $('#slider2').slider({
 	range: 'min'
 });
+
+
+
+
+
+
+
+})();
