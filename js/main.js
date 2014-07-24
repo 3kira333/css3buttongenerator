e91d8dbbbff3676ec556a4cc83965b5b38995b64
Andrey Korshunov <dron1702-1997@bk.ru>
2014-07-24 11:35:26 +0300

    add aditional functional to js and change some styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 (function() {
 
 //variables
-var result = $('#result');
+var result = $('#result'),
+		textInput = $('#text-form'),
+		cssResultArea = $('#cssCode'),
+		htmlResultArea = $('#htmlCode');
 
 //activate slider of border radius
 $('#slider1').slider({
@@ -14,7 +17,8 @@ $('#slider1').slider({
 		var newRadius = ui.value;
 		result.css({
 			'border-radius' : newRadius
-		})
+		});
+		updateResult();
 	}
 });
 
@@ -30,10 +34,38 @@ $('#slider2').slider({
 		var newSize = ui.value;
 		result.css({
 			'border-width' : newSize
-		})
+		});
+		updateResult();
 	}
 });
 
+textInput.on('change', function() {
+	var newText = $(this).val();
+	console.log(newText);
+	$(result).text(newText);
+	updateResult();
+})
+
+var updateResult = function() {
+//get new css and html
+	var bdRadius = result.css('border-radius'),
+			bdRoundRadius = parseInt(bdRadius, 10),
+			bdSize = result.css('border-width'),
+			txtInput = result.text();
+// put in css code
+	cssResultArea.text(
+				'-webkit-border-radius: ' + bdRoundRadius + 'px;\n' +
+				'-moz-border-radius: ' + bdRoundRadius + 'px;\n'+
+				'-ms-border-radius: ' + bdRoundRadius + 'px;\n' +
+				'-o-border-radius: ' + bdRoundRadius + 'px;\n' +
+				'border-radius: ' + bdRoundRadius + 'px;\n' +
+				'border-width: ' + bdSize + ';\n'
+	);
+// put in html code
+	htmlResultArea.text(
+				'<button>' + txtInput + '</button>\n'
+	)
+};
 
 
 })();
